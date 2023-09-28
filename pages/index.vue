97df05bc2162547d750080331ea0cb67ad5aeed5
Hoàng Anh Lâm <comficker@gmail.com>
2023-09-28 16:42:30 +0700

    Init

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,13 @@ const connect = async () => {
   }
 }
 
+const bet = ref(0)
+const amount = ref(1)
+
+const double = () => {
+
+}
+
 onMounted(() => {
   const tonConnectUI = new TonConnectUI({
     manifestUrl: 'https://tondouble.vercel.app/tonconnect-manifest.json',
@@ -32,12 +39,39 @@ onMounted(() => {
 </script>
 
 <template>
-  <div id="webapp" class="max-w-lg mx-auto p-2">
+  <div id="webapp" class="max-w-lg mx-auto px-2 md:py-2">
     <div class="space-y-2">
-      <div v-if="!connected" class="btn shadow font-semibold rounded-lg p-4 flex justify-between items-center" @click="connect">
+      <div class="grid gap-2 grid-cols-2">
+        <div class="btn shadow font-semibold rounded-lg p-4 flex justify-center" :class="{'primary': bet === 0}" @click="bet = 0">
+          <div>Head</div>
+        </div>
+        <div class="btn shadow font-semibold rounded-lg p-4 flex justify-center" :class="{'primary': bet === 1}" @click="bet = 1">
+          <div>Tail</div>
+        </div>
+      </div>
+      <div class="grid gap-2 grid-cols-3 text-sm">
+        <div
+          v-for="i in [1, 2, 3, 5, 8, 10]"
+          class="btn shadow font-semibold rounded-lg p-4 flex justify-center"
+          :class="{'primary': amount === i}"
+          @click="amount = i"
+        >
+          <div>{{i}} TON</div>
+        </div>
+      </div>
+      <div v-if="!connected" class="btn primary shadow font-semibold rounded-lg p-4 flex justify-between items-center" @click="connect">
         <div class="flex items-center gap-3">
           <div class="i-con-wallet w-5 h-5"/>
           <div>Connect wallet</div>
+        </div>
+        <div>
+          <div class="i-con-right w-5 h-5"/>
+        </div>
+      </div>
+      <div v-else class="btn primary shadow font-semibold rounded-lg p-4 flex justify-between items-center" @click="double">
+        <div class="flex items-center gap-3">
+          <div class="i-con-wallet w-5 h-5"/>
+          <div>Get double</div>
         </div>
         <div>
           <div class="i-con-right w-5 h-5"/>
