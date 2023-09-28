42fb482b22dc852eee01475132f4bcdac0b557fa
Hoàng Anh Lâm <comficker@gmail.com>
2023-09-28 15:34:34 +0700

    Init

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,42 @@
 <script setup lang="ts">
+const route = useRoute()
 
+const h = decodeURIComponent(route.hash)
+const matches = h.split("tgWebAppThemeParams=")
+const themeVariables = ref({
+  "bg_color": "#ffffff",
+  "text_color": "#000000",
+  "hint_color": "#707579",
+  "link_color": "#3390ec",
+  "button_color": "#3390ec",
+  "button_text_color": "#ffffff",
+  "secondary_bg_color": "#f4f4f5"
+})
+if (matches.length == 2) {
+  themeVariables.value = JSON.parse(matches[1])
+}
+
+document.body.style.setProperty('--bg_color', themeVariables.value.bg_color)
+document.body.style.setProperty('--text_color', themeVariables.value.text_color)
+
+useHead({
+  title: "Lobby"
+})
 </script>
 
 <template>
-  <div>
+  <div
+    style="--bg_color: #ffffff"
+    :style='{
+      "--bg_color": themeVariables.bg_color,
+      "--text_color": themeVariables.text_color,
+      "--hint_color": themeVariables.hint_color,
+      "--link_color": themeVariables.link_color,
+      "--button_color": themeVariables.button_color,
+      "--button_text_color": themeVariables.button_text_color,
+      "--secondary_bg_color": themeVariables.secondary_bg_color,
+    }'
+  >
     <slot/>
   </div>
 </template>
-
-<style scoped>
-
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,25 @@
 <script setup lang="ts">
-const route = useRoute()
+import {TonConnectUI} from '@tonconnect/ui'
 
-const h = decodeURIComponent(route.hash)
-const matches = h.split("tgWebAppThemeParams=")
-const themeVariables = ref({
-  "bg_color": "#ffffff",
-  "text_color": "#000000",
-  "hint_color": "#707579",
-  "link_color": "#3390ec",
-  "button_color": "#3390ec",
-  "button_text_color": "#ffffff",
-  "secondary_bg_color": "#f4f4f5"
-})
-if (matches.length == 2) {
-  themeVariables.value = JSON.parse(matches[1])
+const tonConnectUI = new TonConnectUI({
+  manifestUrl: 'https://tondouble.vercel.app/tonconnect-manifest.json',
+});
+
+tonConnectUI.uiOptions = {
+  actionsConfiguration: {
+    twaReturnUrl: 'https://t.me/TonDoubleBot/play'
+  }
 }
 
-document.body.style.setProperty('--bg_color', themeVariables.value.bg_color)
-document.body.style.setProperty('--text_color', themeVariables.value.text_color)
-
+const connect = async () => {
+  const connectedWallet = await tonConnectUI.connectWallet();
+  // Do something with connectedWallet if needed
+  console.log(connectedWallet);
+}
 </script>
 
 <template>
-  <div
-    id="webapp"
-    class="max-w-lg mx-auto p-2"
-    style="--bg_color: #ffffff"
-    :style='{
-      "--bg_color": themeVariables.bg_color,
-      "--text_color": themeVariables.text_color,
-      "--hint_color": themeVariables.hint_color,
-      "--link_color": themeVariables.link_color,
-      "--button_color": themeVariables.button_color,
-      "--button_text_color": themeVariables.button_text_color,
-      "--secondary_bg_color": themeVariables.secondary_bg_color,
-    }'>
+  <div id="webapp" class="max-w-lg mx-auto p-2">
     <div class="space-y-2">
       <div class="btn shadow font-semibold rounded-lg p-4 flex justify-between items-center">
         <div class="flex items-center gap-3">
@@ -55,8 +40,18 @@ document.body.style.setProperty('--text_color', themeVariables.value.text_color)
         </div>
       </div>
     </div>
+    <div class="fixed bottom-0 right-0 left-0">
+      <div class="max-w-lg mx-auto py-4">
+        <div class="btn shadow font-semibold rounded-lg p-4 flex justify-between items-center" @click="connect">
+          <div class="flex items-center gap-3">
+            <div class="i-con-help w-5 h-5"/>
+            <div>Connect wallet</div>
+          </div>
+          <div>
+            <div class="i-con-right w-5 h-5"/>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<style>
-</style>
