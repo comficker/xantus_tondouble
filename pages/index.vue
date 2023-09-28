e4e90f6e8fbda55e839b997c34b73a0a0e9073da
Hoàng Anh Lâm <comficker@gmail.com>
2023-09-28 15:52:49 +0700

    Init

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,48 @@
 <script setup lang="ts">
 import {TonConnectUI} from '@tonconnect/ui'
 
-const tonConnectUI = new TonConnectUI({
-  manifestUrl: 'https://tondouble.vercel.app/tonconnect-manifest.json',
-});
 
-tonConnectUI.uiOptions = {
-  actionsConfiguration: {
-    twaReturnUrl: 'https://t.me/TonDoubleBot/play'
+const connector = ref<TonConnectUI | null>(null)
+const connected = ref(false)
+
+const connect = async () => {
+  if (connector.value && !connector.value.connected) {
+    const connectedWallet = await connector.value.connectWallet();
+    console.log(connectedWallet);
   }
 }
 
-const connect = async () => {
-  const connectedWallet = await tonConnectUI.connectWallet();
-  // Do something with connectedWallet if needed
-  console.log(connectedWallet);
-}
+onMounted(() => {
+  const tonConnectUI = new TonConnectUI({
+    manifestUrl: 'https://tondouble.vercel.app/tonconnect-manifest.json',
+  });
+
+  tonConnectUI.uiOptions = {
+    actionsConfiguration: {
+      twaReturnUrl: 'https://t.me/TonDoubleBot/play'
+    }
+  }
+
+  tonConnectUI.connectionRestored.then((r) => {
+    connected.value = r
+  })
+
+  connector.value = tonConnectUI
+})
 </script>
 
 <template>
   <div id="webapp" class="max-w-lg mx-auto p-2">
     <div class="space-y-2">
+      <div v-if="!connected" class="btn shadow font-semibold rounded-lg p-4 flex justify-between items-center" @click="connect">
+        <div class="flex items-center gap-3">
+          <div class="i-con-wallet w-5 h-5"/>
+          <div>Connect wallet</div>
+        </div>
+        <div>
+          <div class="i-con-right w-5 h-5"/>
+        </div>
+      </div>
       <div class="btn shadow font-semibold rounded-lg p-4 flex justify-between items-center">
         <div class="flex items-center gap-3">
           <div class="i-con-rank w-5 h-5"/>
@@ -37,19 +59,6 @@ const connect = async () => {
         </div>
         <div>
           <div class="i-con-right w-5 h-5"/>
-        </div>
-      </div>
-    </div>
-    <div class="fixed bottom-0 right-0 left-0">
-      <div class="max-w-lg mx-auto py-4">
-        <div class="btn shadow font-semibold rounded-lg p-4 flex justify-between items-center" @click="connect">
-          <div class="flex items-center gap-3">
-            <div class="i-con-help w-5 h-5"/>
-            <div>Connect wallet</div>
-          </div>
-          <div>
-            <div class="i-con-right w-5 h-5"/>
-          </div>
         </div>
       </div>
     </div>
